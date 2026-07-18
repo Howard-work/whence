6ee494074a4c33691b964a32a94c84afd64be9fe
Howard-work <durian96@gmail.com>
2026-07-18 11:57:29 +0800

    fix: restore mobile report scrolling

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict';
-const APP_VERSION = '1.3.0';
+const APP_VERSION = '1.3.1';
 
 const SHANFANG_COPY = {
   daily: [
@@ -3209,7 +3209,7 @@ function init() {
   });
 
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js?v=1.3.0').catch(() => {});
+    navigator.serviceWorker.register('./sw.js?v=1.3.1').catch(() => {});
   }
 
   resetCalendarForm();
